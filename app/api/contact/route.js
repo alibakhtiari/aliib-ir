e41ba7ab9b2573281ae238c68f89c7738a57dbda
Ali Bakhtiari <ali.bakhtiarii@gmail.com>
2025-10-10 15:11:55 +0330

    fixing mail send

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,11 +1,11 @@
 import { NextResponse } from 'next/server'
 import { Resend } from 'resend'
 
-const resend = new Resend(process.env.RESEND_API_KEY)
-
 export async function POST(request) {
   try {
     const { name, email, subject, message } = await request.json()
+
+    const resend = new Resend(process.env.RESEND_API_KEY)
 
     // Send email using Resend
     const data = await resend.emails.send({
